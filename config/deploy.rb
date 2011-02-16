34c76fa6fbfb3990500ac452ab4f665b87a1d11d
Tobias Bielohlawek <tobi@soundcloud.com>
2011-02-16 23:01:17 +0100

    init capistrano

---
+++ config/deploy.rb
@@ -0,0 +1,54 @@
+
+$:.unshift(File.expand_path('./lib', ENV['rvm_path']))
+
+require 'rvm/capistrano'
+require 'bundler/capistrano'
+
+set :application, "faceorama"
+
+set :use_sudo, false
+set :user, 'ssh-21560'
+
+set :rvm_type, :user
+set :rvm_ruby_string, "ruby-1.9.2-p136"
+
+# If you aren't deploying to /u/apps/#{application} on the target
+# servers (which is the default), you can specify the actual location
+# via the :deploy_to variable:
+set :deploy_to, "/kunden/warteschlange.de/produktiv/rails/#{application}"
+
+# If you aren't using Subversion to manage your source code, specify
+# your SCM below:
+set :scm, :git
+set :repository, "git://github.com/rngtng/#{application}.git"
+set :branch, "master"
+
+set :deploy_via, :remote_cache
+
+set :ssh_options, :forward_agent => true
+
+role :app, "#{application}.warteschlange.de"
+role :web, "#{application}.warteschlange.de"
+role :db,  "#{application}.warteschlange.de", :primary => true
+
+namespace :deploy do
+  desc "Restarting mod_rails with restart.txt"
+  task :restart, :roles => :app, :except => { :no_release => true } do
+    run "touch #{current_path}/tmp/restart.txt"
+  end
+
+  desc "Link in the production database.yml"
+  task :link_configs do
+    run "ln -nfs #{deploy_to}/#{shared_dir}/database.yml #{release_path}/config/database.yml"
+  end
+
+  [:start, :stop].each do |t|
+    desc "#{t} task is a no-op with mod_rails"
+    task t, :roles => :app do ; end
+  end
+end
+
+
+after "deploy:update_code" do
+  deploy.link_configs
+end

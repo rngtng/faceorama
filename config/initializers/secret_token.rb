6775a658fdf0cd9750d20398daf4c4b6c223ecc1
Tobias Bielohlawek <tobi@soundcloud.com>
2011-02-16 22:35:06 +0100

    init rails

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Faceorama::Application.config.secret_token = 'c709dd8c3c1202b50e421b9f0622c8408d6db12c1a83906dc8efecd2844aa1d2bcac553ce54f88032f3a483681d94f3988abbddc9dc4f4caa7cf9b6dc4d05e5b'

2e8f0cf1860e4eb396547654cb32f008d295cb2d
Tobias Bielohlawek <tobi@soundcloud.com>
2011-02-18 14:54:23 +0100

    optimized image uploading and tagging

--- app/models/image.rb
+++ app/models/image.rb
@@ -1,21 +1,29 @@
 
 class Image < ActiveRecord::Base
   include ImageCropper
-  include FacebookUploader
 
   has_attached_file :photo
 
   validates_presence_of :photo_file_name
-  
-  serialize :crop_data
-  
+
   RATIO = 493.0 / 68.0
 
   def process
     files = ImageCropper.crop(self.photo.path, crop_x, crop_y, crop_width, crop_height)
-    obj = FacebookUploader.upload(facebook_token, files.reverse, self.facebook_uid)
-    FacebookUploader.tag(facebook_token, obj, self.facebook_uid, tag_uid)
+
+    files.reverse.each do |file|
+      upload_and_tag(file)
+    end
   end
-  
+
+  def upload_and_tag(file)
+    file_hash = { 'path' => file, 'content_type' => 'image/jpeg' }
+    tag       = { 'tag_uid' => self.tag_uid.to_s, :x => 10, :y => 10 }
+    api.put_picture(file_hash, {'tags' => [tag].to_json })
+  end
+
+  def api
+    @api ||= Koala::Facebook::GraphAPI.new(self.facebook_token)
+  end
 
 end
